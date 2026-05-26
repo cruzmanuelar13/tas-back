bc8701ebbd8d9061006fe6684221fd42a24a7ea4
cmanuelcabbage <cmanuel.cabbage@gmail.com>
2026-05-25 21:36:45 -0500

    Configuracion de cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(
+    AppModule,
+  );
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
+    credentials: true,
+  });
+
+  await app.listen(
+    process.env.PORT ?? 3001,
+  );
 }
+
 bootstrap();
